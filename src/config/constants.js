61a7d526090ad2be706039d5a3934146e0a97052
Dan Iselin <you@example.com>
2021-09-17 11:38:08 -0500

    update api url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
 // export const API_URL = "http://127.0.0.1:8000/api";
-export const API_URL = "https://pencilmark-bff.herokuapp.com/";
+export const API_URL = "https://pencilmark-bff.herokuapp.com/api";
 export const RESET_STATE = "app/RESET_STATE";
